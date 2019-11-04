c96c1cfbfc562bee194fc07e9ab6838bafa8756c
Zaidane-E <elhaouarizaidane@gmail.com>
2019-11-04 08:23:48 -0500

    Made adjustments to preloader

--- loading.js
+++ loading.js
@@ -2,7 +2,6 @@ var preloader = document.getElementById('preloader')
 var page = document.getElementById('page')
 
 function load(){
-    preloader.style.display = "none"
     page.style.display = "block"
 }
 
